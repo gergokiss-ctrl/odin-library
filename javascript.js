df91ce72c87a6b7a9c05882da31772303ce086be
gergokiss-ctrl <kiss.gergo55@gmail.com>
2022-12-29 18:43:49 +0100

    Create HTML content adding function

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ const author = document.querySelector('.author');
 const pages = document.querySelector('.pages');
 const read = document.querySelector('.read');
 const submit = document.querySelector('.submit');
+const container = document.querySelector('.container');
 
 const myLibrary = [];
 let counter = 0;
@@ -19,6 +20,31 @@ function addBookToLibrary() {
     if (title.value !== '' && author.value !== '' && pages.value !== '') {
       myLibrary[counter] = new Book(title.value, author.value, pages.value, read.checked);
       counter += 1;
+
+      const content = document.createElement('div');
+      content.classList.add('content');
+
+      const titleText = document.createElement('div');
+      const authorText = document.createElement('div');
+      const pagesText = document.createElement('div');
+      const readText = document.createElement('div');
+
+      titleText.textContent = `"${title.value}"`;
+      authorText.textContent = author.value;
+      pagesText.textContent = pages.value;
+
+      if (read.checked === true) {
+        readText.textContent = 'Read';
+      } else {
+        readText.textContent = 'Not read';
+      }
+
+      content.appendChild(titleText);
+      content.appendChild(authorText);
+      content.appendChild(pagesText);
+      content.appendChild(readText);
+
+      container.appendChild(content);
     }
   });
 }
