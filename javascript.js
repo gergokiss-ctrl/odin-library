8873e680c2940ade4b61c528c67cfa50926b9dfc
gergokiss-ctrl <kiss.gergo55@gmail.com>
2022-12-29 17:43:40 +0100

    Create addBookToLibrary function
    The function inserts the user inputs into the last unused array element.
    The input objects have each been given a class.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,26 @@
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const pages = document.querySelector('.pages');
+const read = document.querySelector('.read');
+const submit = document.querySelector('.submit');
+
+const myLibrary = [];
+let counter = 0;
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
+
+function addBookToLibrary() {
+  submit.addEventListener('click', () => {
+    if (title.value !== '' && author.value !== '' && pages.value !== '') {
+      myLibrary[counter] = new Book(title.value, author.value, pages.value, read.checked);
+      counter += 1;
+    }
+  });
+}
+
+addBookToLibrary();
